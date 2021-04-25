d3de9e27471a36a06be143c4396cd15e9cb2ae66
David Tyler <david.tyler@metaswitch.com>
2021-04-25 16:38:51 +0100

    Added interruptsEnabled flag to cpu state across whole architecture

--- test.sh
+++ test.sh
@@ -5,9 +5,9 @@ sleep 5
 
 RESULT=`curl -s --header "Content-Type: application/json" \
   --request POST \
-  --data '{"id":"abcd", "opcode":168,"state":{"a":92,"b":120,"c":66,"d":5,"e":15,"h":10,"l":2,"flags":{"sign":false,"zero":false,"auxCarry":true,"parity":false,"carry":true},"programCounter":1,"stackPointer":2,"cycles":0}}' \
+  --data '{"id":"abcd", "opcode":168,"state":{"a":92,"b":120,"c":66,"d":5,"e":15,"h":10,"l":2,"flags":{"sign":false,"zero":false,"auxCarry":true,"parity":false,"carry":true},"programCounter":1,"stackPointer":2,"cycles":0,"interruptsEnabled":true}}' \
   http://localhost:8080/api/v1/execute`
-EXPECTED='{"id":"abcd", "opcode":168,"state":{"a":36,"b":120,"c":66,"d":5,"e":15,"h":10,"l":2,"flags":{"sign":false,"zero":false,"auxCarry":false,"parity":true,"carry":false},"programCounter":1,"stackPointer":2,"cycles":4}}'
+EXPECTED='{"id":"abcd", "opcode":168,"state":{"a":36,"b":120,"c":66,"d":5,"e":15,"h":10,"l":2,"flags":{"sign":false,"zero":false,"auxCarry":false,"parity":true,"carry":false},"programCounter":1,"stackPointer":2,"cycles":4,"interruptsEnabled":true}}'
 
 docker kill xra
 
